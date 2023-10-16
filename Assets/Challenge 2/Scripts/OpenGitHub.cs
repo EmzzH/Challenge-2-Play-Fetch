1e8878f1dc657b628a856a800101049225d65f77
EmzzH <145970013+EmzzH@users.noreply.github.com>
2023-10-16 19:35:57 +0100

    zdgh

--- Assets/Challenge 2/Scripts/OpenGitHub.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class OpenGitHub : MonoBehaviour
-
-{ Application.OpenUrl(“https://github.com/EmzzH/Challenge-2-Play-Fetch/tree/0060620aee75659183e5fab357fa540077de1d11/Assets/Challenge%202/Scripts”); }
